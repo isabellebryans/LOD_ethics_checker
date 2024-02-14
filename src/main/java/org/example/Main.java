0f48be9c160bc14ab842175133741e99e3f963d3
isabellebryans <bryansi@tcd.ie>
2024-02-14 14:16:09 +0000

    Check rdfs:label for vulnerable groups

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,44 @@
 package org.example;
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.example.loadData;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Model model = loadData.initAndLoadModelFromResource("czso-job-applicants-and-unemployment-rate-vocabulary.ttl", Lang.TURTLE);
+        RDFDataMgr.write(System.out, model, Lang.TTL);
+        String queryString =
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+                "SELECT * " +
+                "WHERE{" +
+                        "?s rdfs:label ?o." +
+                        "FILTER (CONTAINS(?o, 'sex') || CONTAINS(?o, 'unemploy'))" +
+                        "}";
+        System.out.println(queryString);
+        Query query = QueryFactory.create(queryString);
+        QueryExecution qexec = QueryExecutionFactory.create(query, model);
+        try{
+            ResultSet results = qexec.execSelect();
+            while (results.hasNext()){
+                QuerySolution solution = results.nextSolution();
+                Resource name = solution.getResource("s");
+                System.out.println(name);
+            }
+        } finally {
+            qexec.close();
         }
     }
 }
